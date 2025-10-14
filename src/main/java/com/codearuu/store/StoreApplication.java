9ed3e78f8662f21208a4334202a0cc33dbfc117f
Aruuke Turgunbaeva <nyc.aruu@gmail.com>
2025-10-14 15:42:03 +0200

    Managing transactions

--- src/main/java/com/codearuu/store/StoreApplication.java
+++ src/main/java/com/codearuu/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.codearuu.store;
 
 import com.codearuu.store.entities.User;
 import com.codearuu.store.repositories.UserRepository;
+import com.codearuu.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +12,7 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var repository = context.getBean(UserRepository.class);
-
-        repository.findAll().forEach(u -> System.out.println(u.getEmail()));
-        repository.deleteById(1L);
-        repository.deleteAll();
+        var service = context.getBean(UserService.class);
+        service.showEntityStates();
     }
 }
---
+++ src/main/java/com/codearuu/store/services/UserService.java
@@ -0,0 +1,39 @@
+package com.codearuu.store.services;
+
+import com.codearuu.store.entities.User;
+import com.codearuu.store.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final EntityManager entityManager;
+
+    @Transactional
+    public void showEntityStates(){
+        var user = User.builder()
+                .name("John Doe")
+                .email("john@example.com")
+                .password("password")
+                .build();
+
+        if (entityManager.contains(user)) {
+            System.out.println("Persistent");
+        } else {
+            System.out.println("Transient / Detached");
+        }
+
+        userRepository.save(user);
+
+        if (entityManager.contains(user)) {
+            System.out.println("Persistent");
+        } else {
+            System.out.println("Transient / Detached");
+        }
+    }
+}
