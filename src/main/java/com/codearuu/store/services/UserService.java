6a3dfe192efc8bf881eed00ef3a850e2daa81a8f
Aruuke Turgunbaeva <nyc.aruu@gmail.com>
2025-10-14 16:43:10 +0200

    Persisted related entities

--- src/main/java/com/codearuu/store/services/UserService.java
+++ src/main/java/com/codearuu/store/services/UserService.java
@@ -1,6 +1,8 @@
 package com.codearuu.store.services;
 
+import com.codearuu.store.entities.Address;
 import com.codearuu.store.entities.User;
+import com.codearuu.store.repositories.AddressRepository;
 import com.codearuu.store.repositories.ProfileRepository;
 import com.codearuu.store.repositories.UserRepository;
 import jakarta.persistence.EntityManager;
@@ -15,6 +17,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final ProfileRepository profileRepository;
     private final EntityManager entityManager;
+    private final AddressRepository addressRepository;
 
     @Transactional
     public void showEntityStates(){
@@ -43,5 +46,28 @@ public class UserService {
     public void showRelatedEntities(){
         var profile = profileRepository.findById(8L).orElseThrow();
         System.out.println(profile.getUser().getEmail());
+    }
+
+    public void fetchAddress() {
+        var address = addressRepository.findById(1L).orElseThrow();
+    }
+
+    public void persistRelated() {
+        var user = User.builder()
+                .name("kate Smith")
+                .email("kate@gmail.com")
+                .password("password")
+                .build();
+
+        var address = Address.builder()
+                .street("Main Street")
+                .city("Main City")
+                .state("Main State")
+                .zip("Main Zip")
+                .build();
+
+        user.addAddress(address);
+
+        userRepository.save(user);
     }
 }
